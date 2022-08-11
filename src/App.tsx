cdb1cb199ed6795a7ceb77b8204de950923361d0
Melissa Rose <123melissa321@gmail.com>
2022-08-11 12:31:46 +0100

    Removed deleteThree button that was used for experiment

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,10 @@ function App(): JSX.Element {
     .then((response) => response.json())
     .then((jsonBody: FullToDoItem[]) => setTodos(jsonBody))
   }, [])
-
-  const handleDeleteThree = () => {
-    axios.delete(`http://localhost:4000/items/3`);
-    setTodos(todos.filter((todo) => todo.id !== 3));
-  }
   
   if (todos.length > 0) {
     return (
       <>
-        <button type="button" onClick={handleDeleteThree}>Delete item 3</button>
         <CreateToDo />
         <div className="todo-list">
           {todos.map(todo => <ToDoCard key={todo.id} todos={todos} setTodos={setTodos} id={todo.id} task={todo.task} completed={todo.completed} creationDate={todo.creationDate} dueDate={todo.dueDate} />)}
