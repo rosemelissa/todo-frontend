f302fd26a5ade8ed000a0de15d028debe80113a4
Melissa Rose <123melissa321@gmail.com>
2022-08-11 19:02:00 +0100

    ADded currentTodo state and option to edit todos

--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { FullToDoItem } from './types';
+import {useEffect, useState} from 'react';
 
 interface ToDoCardProps {
     todos: FullToDoItem[];
@@ -12,7 +13,18 @@ interface ToDoCardProps {
 }
 
 function ToDoCard({todos, setTodos, id, task, completed, creationDate, dueDate}: ToDoCardProps): JSX.Element {
-    
+    const [mode, setMode] = useState<'view' | 'edit'>('view')
+    const [currentTodo, setCurrentTodo] = useState<FullToDoItem>({
+        "id": id,
+        "task": task,
+        "completed": completed,
+        "creationDate": creationDate,
+        "dueDate": dueDate})
+
+    // useEffect(() => {
+
+    // }, [currentTodo])
+
     const handleDelete = () => {
         axios.delete(`http://localhost:4000/items/${id}`);
         setTodos(todos.filter((todo) => (todo.id !== id)))
@@ -42,18 +54,56 @@ function ToDoCard({todos, setTodos, id, task, completed, creationDate, dueDate}:
         }
     }
 
-    return (
-        <div className="to-do-card">
-            <h1>{id}</h1>
-            <h1>{task}</h1>
-            {completed 
-            ? <input type="checkbox" checked onClick={handleMakeIncomplete}/> 
-            : <input type="checkbox" onClick={handleMakeComplete}/>}
-            <button type="button" onClick={handleDelete}>Delete</button>
-            <p>Created: {creationDate}</p>
-            <p>Due: {dueDate}</p>
-        </div>
-    )
+    function handleUpdate() {
+        axios.patch(`http://localhost:4000/items/${id}`, {task: currentTodo.task, dueDate: currentTodo.dueDate});
+        setTodos(todos.map(todo => changeCurrentTodo(todo)))
+    }
+
+    function changeCurrentTodo(todo: FullToDoItem) {
+        if (todo.id === id) {
+            return {
+                "id": todo.id, 
+                "task": currentTodo.task,
+                "completed": !(todo.completed),
+                "creationDate": todo.creationDate,
+                "dueDate": currentTodo.dueDate
+            }
+        } else {
+            return todo;
+        }
+    }
+
+    if (mode === 'view') {
+        return (
+            <div className="view-to-do-card">
+                <h1>{id}</h1>
+                <h1>{task}</h1>
+                {completed 
+                ? <input type="checkbox" checked onClick={handleMakeIncomplete}/> 
+                : <input type="checkbox" onClick={handleMakeComplete}/>}
+                <button type="button" onClick={handleDelete}>Delete</button>
+                <button type="button" onClick={() => setMode('edit')}>Edit</button>
+                <p>Created: {creationDate}</p>
+                <p>Due: {dueDate}</p>
+            </div>
+        )
+    } else if (mode === 'edit') {
+        return (
+            <div className='edit-to-do-card'>
+                <h1>{id}</h1>
+                <form onSubmit={handleUpdate}>
+                    <input type="text" value={currentTodo.task} onChange={(e) => setCurrentTodo(prevState => ({...prevState, task: e.target.value}))}/>
+                    <input type='submit' value='Update'/>
+                </form>
+                
+            </div>
+        )
+    } else {
+        return (
+            <h1>Empty</h1>
+        )
+    }
+    
 }
 
 export default ToDoCard;
